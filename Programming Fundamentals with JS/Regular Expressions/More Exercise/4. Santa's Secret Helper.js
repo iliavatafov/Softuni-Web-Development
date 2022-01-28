daee894f970c3db14bb334235aa3a6b74f8dceb7
ilia vatafov <iliyavatafov@gmail.com>
2022-01-28 11:29:34 +0200

    RegEx update

--- Programming Fundamentals with JS/Regular Expressions/More Exercise/4. Santa's Secret Helper.js
+++ Programming Fundamentals with JS/Regular Expressions/More Exercise/4. Santa's Secret Helper.js
@@ -16,16 +16,24 @@ function solve(input) {
         decryptedMessages.push(currentMessage);
         decryptedLine = input.shift()
     }
-    let pattern = /@(?<name>[A-Za-z]+)([^-!@\:>]+)?!(?<goodOrBad>[GN])!/g
+    let pattern = /@(?<name>[A-Za-z]+)(?:[^-!\:>]+)?!(?<goodOrBad>[G])!/
 
     let listOfGoodKids = [];
 
     for (let message of decryptedMessages) {
-        let match = pattern.exec(decryptedMessages);
-        if (match !== null && match[3] === `G`) {
+        let match = pattern.exec(message);
+        if (match !== null) {
             listOfGoodKids.push(match[1])
         }
+            
     }
 
     console.log(listOfGoodKids.join(`\n`));
 }
+
+solve(['3',
+'CNdwhamigyenumje$J$',
+'CEreelh-nmguuejnW$J$',
+'CVwdq&gnmjkvng$Q$',
+'end']
+)
