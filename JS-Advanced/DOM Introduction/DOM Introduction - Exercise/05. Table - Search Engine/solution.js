ade9964e4d895cf9dec7ade77be0fdb9b6cc3927
ilia vatafov <iliyavatafov@gmail.com>
2022-02-12 10:33:51 +0200

    commit

--- JS-Advanced/DOM Introduction/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS-Advanced/DOM Introduction/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,24 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   }
+    function onClick() {
+
+        let rowsToBeChecked = document.querySelectorAll(`tbody tr`);
+        let input = document.querySelector(`#searchField`);
+
+        for (let row of rowsToBeChecked) {
+
+            row.classList.remove(`select`);
+            let rowElements = row.querySelectorAll(`td`);
+
+            for (let currentElement of rowElements) {
+
+                if (input.value !== `` && currentElement.textContent.includes(input.value)) {
+                    row.className = `select`;
+                }
+            }
+        }
+        input.value = ``;
+    }
 }
