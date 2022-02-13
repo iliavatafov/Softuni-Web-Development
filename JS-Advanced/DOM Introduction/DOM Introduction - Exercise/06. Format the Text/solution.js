1dc79f45b80a9b522adcb54617f0f4a605c102d7
ilia vatafov <iliyavatafov@gmail.com>
2022-02-13 12:14:41 +0200

    commit

--- JS-Advanced/DOM Introduction/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS-Advanced/DOM Introduction/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+
+    let input = document.getElementById(`input`);
+    let output = document.getElementById(`output`);
+    output.textContent = ``;
+
+    let arrFromInput = input.value.split(`.`).filter(x => x !== ``);
+
+    while (arrFromInput.length > 0) {
+
+        let text = arrFromInput.splice(0, 3).join(`. `) + `.`;
+        let p = document.createElement(`p`);
+        p.textContent = text;
+        output.appendChild(p);
+    }
 }
--- JS-Advanced/DOM Introduction/DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS-Advanced/DOM Introduction/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,30 @@
 function generateReport() {
-    //TODO
+
+    let colsHeaderData = Array.from(document.querySelectorAll(`thead tr th input`));
+    let numberOfRows = document.querySelectorAll(`tbody tr td:nth-child(1)`).length;
+
+    let resultArray = [];
+
+
+
+    for (let i = 0; i < numberOfRows; i++) {
+
+        let currentObject = {};
+
+        for (let heading of colsHeaderData) {
+
+            if (heading.checked) {
+                let trChaildNumber = colsHeaderData.indexOf(heading) + 1;
+                let colData = document.querySelectorAll(`tbody tr td:nth-child(${trChaildNumber}`);
+                let propertyName = heading.name;
+                let propertyValue = colData[i].textContent;
+                currentObject[propertyName] = propertyValue;
+            }
+        }
+
+        resultArray.push(currentObject);
+    }
+
+    document.getElementById('output').value = JSON.stringify(resultArray);
+
 }
