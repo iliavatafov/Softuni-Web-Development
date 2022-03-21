0124785c564dbe6140548923ad7b2d3c7a04076c
ilia vatafov <iliyavatafov@gmail.com>
2022-03-21 07:26:56 +0200

    commit

--- JS-Advanced/Exam Preparation 3/02. Vegetable store/02. Vegetable store.js
+++ JS-Advanced/Exam Preparation 3/02. Vegetable store/02. Vegetable store.js
@@ -109,12 +109,3 @@ class VegetableStore {
         return result.join(`\n`);
     }
 }
-
-
-let vegStore = new VegetableStore('Jerrie Munro', '1463 Pette Kyosheta, Sofia');
-
-console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8", "Celery 5.5 2.2", "Celery 0.5 2.5"]))
-console.log(vegStore.rottingVegetable("Okra", 1))
-console.log(vegStore.rottingVegetable("Okra", 2.5))
-console.log(vegStore.buyingVegetables(["Beans 8", "Celery 1.5"]))
-console.log(vegStore.revision())
