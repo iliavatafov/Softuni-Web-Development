f349886bdf81761e5cc275b15e9eb97a2dabd070
ilia vatafov <iliyavatafov@gmail.com>
2022-03-25 17:57:12 +0200

    commit

--- JS-Applications/Asynchronous Programming/03.Gighub-Commits/app.js
+++ JS-Applications/Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,48 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+
+    const usernameElement = document.querySelector(`#username`);
+    const repoElement = document.querySelector(`#repo`);
+    const commits = document.querySelector(`#commits`);
+    const btn = document.querySelector(`button`);
+
+    btn.addEventListener(`click`, load)
+
+    function load(ev) {
+
+        ev.preventDefault();
+
+        const url = `https://api.github.com/repos/${usernameElement.value}/${repoElement.value}/commits`;
+
+        takeRepos(url);
+    }
+
+    async function takeRepos(url) {
+
+        try {
+
+            const response = await fetch(url);
+
+            if (response.ok !== true) {
+                throw new Error(`Error: ${response.status} (${response.statusText})`);
+            }
+
+            const data = await response.json();
+
+            commits.innerHTML = ``;
+
+            for (let commitData of data) {
+                const newLi = document.createElement(`li`);
+                newLi.textContent = `${commitData.commit.author.name} : ${commitData.commit.message}`;
+                commits.appendChild(newLi);
+            }
+
+        } catch (err) {
+            commits.innerHTML = ``;
+            const liElement = document.createElement(`li`);
+            liElement.textContent = `${err.message}`;
+            commits.appendChild(liElement);
+        }
+
+    }
+
 }
