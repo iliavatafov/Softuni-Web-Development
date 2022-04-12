dfc03755dd13a49b3131aa9e0a7b6dd2bda68717
ilia vatafov <iliyavatafov@gmail.com>
2022-04-12 13:53:12 +0300

    commit

--- JS-Applications/Remote Data and Authentication/Exercise/03.Students/app.js
+++ JS-Applications/Remote Data and Authentication/Exercise/03.Students/app.js
@@ -1,1 +1,117 @@
-console.log('TODO...');
+window.addEventListener(`load`, () => {
+    displayData();
+
+    const form = document.querySelector(`form`);
+    form.addEventListener(`submit`, onCreate)
+
+
+});
+
+async function onCreate(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.target);
+
+    let firstName = formData.get(`firstName`).trim();
+    let lastName = formData.get(`lastName`).trim();
+    let facultyNumber = formData.get(`facultyNumber`).trim();
+    let grade = formData.get(`grade`).trim();
+
+    if (firstName != `` && lastName != `` && facultyNumber !=
+        `` && !isNaN(Number(facultyNumber)) && grade != `` && !isNaN(grade)) {
+
+        const url = `http://localhost:3030/jsonstore/collections/students`;
+
+        const options = {
+            method: `post`,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                facultyNumber: facultyNumber,
+                grade: grade
+            })
+        }
+
+        try {
+            const req = await fetch(url, options);
+
+            if (req.status !== 200) {
+                const error = await req.json();
+                throw new Error(error.message);
+            }
+
+            const data = await req.json();
+
+            form.querySelectorAll(`input`).forEach(i => i.value = ``)
+
+            displayData();
+
+        } catch (err) {
+            alert(err.message);
+        }
+    } else {
+        if (isNaN(grade)) {
+            alert(`Grade should be a number!`);
+        } else if (isNaN(Number(facultyNumber))) {
+            alert(`Faculty Number must be a number!`);
+        } else {
+            alert(`All fields must be fieled!`);
+        }
+
+    }
+}
+
+async function displayData() {
+
+    const tableBodyElement = document.querySelector(`tbody`);
+    tableBodyElement.replaceChildren();
+
+    const data = await getData();
+
+    data.forEach(e => {
+        const tr = document.createElement(`tr`);
+
+        const firstNameElement = document.createElement(`th`);
+        firstNameElement.textContent = e.firstName;
+
+        const lastNameElement = document.createElement(`th`);
+        lastNameElement.textContent = e.lastName;
+
+        const facultyNumberElement = document.createElement(`th`);
+        facultyNumberElement.textContent = e.facultyNumber;
+
+        const gradeElement = document.createElement(`th`);
+        gradeElement.textContent = e.grade;
+
+        tr.appendChild(firstNameElement);
+        tr.appendChild(lastNameElement);
+        tr.appendChild(facultyNumberElement);
+        tr.appendChild(gradeElement);
+
+        tableBodyElement.appendChild(tr)
+    });
+}
+
+async function getData() {
+    const url = `http://localhost:3030/jsonstore/collections/students`;
+
+    try {
+
+        const req = await fetch(url);
+
+        if (req.status !== 200) {
+            const error = req.json();
+            throw new Error(error.message);
+        }
+
+        const data = await req.json();
+
+        return Object.values(data);
+
+    } catch (err) {
+        alert(err.message);
+    }
+}
--- JS-Applications/Remote Data and Authentication/Exercise/04.Book-library/app.js
+++ JS-Applications/Remote Data and Authentication/Exercise/04.Book-library/app.js
@@ -1,1 +1,137 @@
-console.log('My requests...')
+const tbody = document.querySelector(`tbody`);
+document.getElementById(`loadBooks`).addEventListener(`click`, loadBooks);
+const createForm = document.getElementById(`createForm`);
+const editForm = document.getElementById(`editForm`);
+createForm.addEventListener(`submit`, onCreate);
+
+loadBooks();
+
+async function loadBooks() {
+
+    const books = await request(`http://localhost:3030/jsonstore/collections/books/`);
+
+    tbody.replaceChildren();
+    Object.entries(books).forEach(e => loadBook(e[1].author, e[1].title, e[0]));
+}
+
+function loadBook(author, title, id) {
+    const tr = document.createElement(`tr`);
+
+    tr.innerHTML = `<td>${title}</td>
+    <td>${author}</td>
+    <td>
+        <button id="editBtn">Edit</button>
+        <button id="deleteBtn">Delete</button>
+    </td>`;
+
+    tr.querySelector(`td #deleteBtn`).addEventListener(`click`, onDelete.bind(null, id));
+    tr.querySelector(`td #editBtn`).addEventListener(`click`, onEdit.bind(null, id));
+
+    tbody.appendChild(tr);
+}
+
+async function onCreate(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.target);
+
+    const author = formData.get(`author`).trim();
+    const title = formData.get(`title`).trim();
+
+    if (author == `` && title == ``) {
+        alert(`Please fill all fileds!`);
+        return;
+    }
+
+    const options = {
+        method: `post`,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ author, title }),
+    };
+
+    await request(`http://localhost:3030/jsonstore/collections/books/`, options);
+    ev.target.reset()
+    loadBooks();
+
+}
+
+async function onDelete(id, ev) {
+
+    request(`http://localhost:3030/jsonstore/collections/books/` + id, {
+        method: `delete`,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    ev.target.parentNode.parentNode.remove();
+
+}
+
+async function onEdit(id, ev) {
+    ev.preventDefault();
+    createForm.style.display = `none`;
+    editForm.style.display = `block`;
+
+    const book = await request(`http://localhost:3030/jsonstore/collections/books/` + id);
+
+    const title = document.querySelector(`#editForm [name="title"]`);
+    const author = document.querySelector(`#editForm [name="author"]`);
+
+    author.value = book.author;
+    title.value = book.title;
+
+    editForm.addEventListener(`submit`, onUpdate.bind(null, id));
+
+}
+
+async function onUpdate(id, ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.target);
+
+    const author = formData.get(`author`).trim();
+    const title = formData.get(`title`).trim();
+
+    if (author == `` && title == ``) {
+        alert(`Please fill all fileds!`);
+        return;
+    }
+
+    const options = {
+        method: `put`,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ author, title }),
+    };
+
+    await request(`http://localhost:3030/jsonstore/collections/books/` + id, options);
+
+    ev.target.reset()
+
+    createForm.style.display = `block`;
+    editForm.style.display = `none`;
+
+    loadBooks();
+}
+
+async function request(url, options) {
+    try {
+        const req = await fetch(url, options);
+
+        if (req.ok !== true) {
+            const error = req.json();
+            throw new Error(error.message);
+        }
+
+        const data = await req.json();
+
+        return data
+
+    } catch (err) {
+        alert(err.message);
+    }
+}
