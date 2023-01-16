bc961e20105d4ad4c0a8031b26cfa0e0e372e97e
ilia vatafov <iliyavatafov@gmail.com>
2023-01-16 11:26:20 +0200

    commit

---
+++ JS Back-End/Node.js Streams and Utilities/pubSub/server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const eventBus = require("./eventBus");
+
+const server = http.createServer((req, res) => {
+  eventBus.publish("request", { method: req.method, url: req.url });
+  res.end();
+});
+
+server.listen(5000);
+console.log("Server listen on port 5000...");
